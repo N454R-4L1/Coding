528a617bdd1b232d14f75651a56c66902527cac0
N454R-4L1 <11.ali.nasar@gmail.com>
2021-10-16 12:47:44 +0530

    New Program

--- Java/arrayst.java
+++ Java/arrayst.java
@@ -8,13 +8,13 @@ public class arrayst
         int n;
         n = sc.nextInt();
 
-        String st[] = new String[n];
+        String st[] = new String[n+1];
 
-        for (int i = 0; i <n; i++) 
+        for (int i = 0; i <st.length; i++) 
         {   
             st[i] = sc.nextLine();
         }
-        for (int i = 0; i <n; i++) 
+        for (int i = 0; i <st.length; i++) 
         {   
             System.out.println(st[i]);
         }
